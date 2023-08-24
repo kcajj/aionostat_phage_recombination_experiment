01d7be4f49da3c8d1ad609ac042cebad43e25933
Giacomo <thejack2307@gmail.com>
2023-08-24 23:11:29 +0200

    Update mutation_densisty.py

--- scripts/mutation_densisty.py
+++ scripts/mutation_densisty.py
@@ -1,4 +1,5 @@
 import pysam
+from Bio import SeqIO
 import numpy as np
 
 def analyse_bam(bam_file, l):
@@ -7,6 +8,15 @@ def analyse_bam(bam_file, l):
         mapping=np.zeros(l,dtype=bool)
         for read in bam.fetch():
             print(read.reference_start)
+            for (read_pos,reference_pos) in read.get_aligned_pairs():
+                if reference_pos!=None and read_pos!=None:
+                    print(read_pos,reference_pos)
+                    print(read.query_sequence[read_pos])
+                    #if ref_seq==read.query_sequence[read_pos]:
+
+                    #add mismatch count
+                    #add mapping positions
+                        
     return mismatches,mapping
 
 if __name__ == "__main__":
@@ -21,8 +31,10 @@ if __name__ == "__main__":
         for isolate in isolates:
             
             #get the length of the isolate genome
+            assembly=f'/home/giacomocastagnetti/code/rec_genome_analysis/results/assemblies/{population}/{isolate}.fasta'
+            seq = SeqIO.read(assembly, "fasta").seq
+            l=len(seq)
 
-            l=120000
             bam_file_path = f'/home/giacomocastagnetti/code/rec_genome_analysis/results/mappings/{population}/{isolate}.bam'
             mismatch_distribution, mapping = analyse_bam(bam_file_path, l)
             print(mismatch_distribution)
